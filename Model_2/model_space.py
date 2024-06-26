1a64f0b86a41aca857d3da9f1271870b8bf39d1d
trihuynh0410 <trihuynh0410@gmail.com>
2024-06-26 20:35:43 +0700

    dareal mobilevit

--- Model_2/model_space.py
+++ Model_2/model_space.py
@@ -110,7 +110,7 @@ class MobileViT(ModelSpace):
     def __init__(
         self,
         num_labels: int = 3,
-        base_widths: Tuple[int, ...] = (32, 16, 32, 40),
+        base_widths: Tuple[int, ...] = (32, 16, 32, 40, 80),
         dropout_rate: float = 0.,
         width_mult: float = 1.0,
         bn_eps: float = 1e-3,
@@ -128,7 +128,7 @@ class MobileViT(ModelSpace):
         rpe: bool = True,
     ):
         super().__init__()
-        assert len(base_widths) == 4
+        assert len(base_widths) == 5
         # include the last stage info widths here
         widths = [make_divisible(width * width_mult, 8) for width in base_widths]
         downsamples = [True, False, True, True, False, True]
